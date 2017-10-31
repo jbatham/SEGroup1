7e22710a03560986c88240940d756ff28beacf14
alhamdubello@hotmail.co.uk <alhamdubello@hotmail.co.uk>
2017-10-31 15:05:25 +0000

    Added the SQL to create table for task3

---
+++ task3.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `atable` (
+  `nl_point_id` INT(11) NOT NULL AUTO_INCREMENT,
+  `lat` DOUBLE DEFAULT 0,
+  `lang` DOUBLE DEFAULT 0,
+  `did` INT NOT NULL,
+  PRIMARY KEY (`nl_point_id`)
+);
