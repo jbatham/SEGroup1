21a4c89ecfb87f5ef9b91df62755a2e5af231a87
Warren Boult <wmboult94@gmail.com>
2017-11-26 16:06:33 +0000

    Test suite now working, no tests written yet

--- node-server/tests/unit/models/prices.js
+++
@@ -1,8 +0,0 @@
-const chai = require('chai');
-const sinon = require('sinon');
-const expect = chai.expect();
-
-describe('get_data', () => {
-	it('should call db.get().query');
-	it('db.get().query should call its callback');
-});
