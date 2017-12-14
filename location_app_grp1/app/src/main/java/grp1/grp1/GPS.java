c0cd173c0c955fdddee0ca4aa73917cc62ad19b0
Warren Boult <wmboult94@gmail.com>
2017-12-14 13:32:24 +0000

    Hacking our way to merge everything together, got app folder

--- location_app_grp1/app/src/main/java/grp1/grp1/GPS.java
+++ location_app_grp1/app/src/main/java/grp1/grp1/GPS.java
@@ -1,10 +1,12 @@
 package grp1.grp1;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.app.Service;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -12,6 +14,8 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
+import android.widget.Toast;
 
 import java.util.concurrent.ExecutionException;
 
@@ -19,35 +23,29 @@ import java.util.concurrent.ExecutionException;
  * Created by Din on 20/10/2017.
  */
 
-public class GPS extends Service implements LocationListener {
+//Class to get GPS location
+public class GPS implements LocationListener {
     //class to handle location and gps services
+    Context context;
 
-    @Nullable
-    @Override
-    public IBinder onBind(Intent intent) {
+    //method called which returns location
+    public Location getLocation(){
+        //checking if user has given location access permission
+        if(ContextCompat.checkSelfPermission(context, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+            Toast.makeText(context,"Location Access not grandted", Toast.LENGTH_LONG).show();
+            return null;
+        }
+        LocationManager lm = (LocationManager)context.getSystemService(Context.LOCATION_SERVICE);
+        boolean isGPSenabled = lm.isProviderEnabled(LocationManager.GPS_PROVIDER);  //checking if GPS enabled
+        if(isGPSenabled){
+            lm.requestLocationUpdates(LocationManager.GPS_PROVIDER,0,0, this);
+            Location location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            return location;
+        }else{
+            Toast.makeText(context,"Please enable GPS", Toast.LENGTH_LONG).show();
+        }
         return null;
     }
-
-    private final Context context;
-    boolean GPSenabled = false;
-    boolean Nenabled = false;
-    boolean getLocation = false;
-    double latitude;
-    double longitude;
-    Location location;
-
-
-    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 0;
-    private static final long MIN_TIME_UPDATES = 1000*60*1;
-
-    protected LocationManager locationManager;
-
-    public GPS(Context context) {
-        this.context = context;
-        getLocation();
-    }
-
-
 
     @Override
     public void onLocationChanged(Location location) {
@@ -55,107 +53,23 @@ public class GPS extends Service implements LocationListener {
     }
 
     @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
+    public void onStatusChanged(String s, int i, Bundle bundle) {
 
     }
 
     @Override
-    public void onProviderEnabled(String provider) {
+    public void onProviderEnabled(String s) {
 
     }
 
     @Override
-    public void onProviderDisabled(String provider) {
+    public void onProviderDisabled(String s) {
 
     }
 
-    public Location getLocation() {
-        try {
-            locationManager = (LocationManager) context.getSystemService(LOCATION_SERVICE);
-            GPSenabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
-            Nenabled = locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
-
-            if(!GPSenabled && !Nenabled) {
-            }else{
-                this.getLocation = true;
-                if(Nenabled){
-                     locationManager.requestLocationUpdates(
-                             LocationManager.NETWORK_PROVIDER,
-                             MIN_TIME_UPDATES,
-                             MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-                if (locationManager != null) {
-                    location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-                    if(location!=null){
-                        latitude= location.getLatitude();
-                        longitude= location.getLongitude();
-                    }
-                }
-            }
-            if(GPSenabled){
-                if(location==null) {
-                    locationManager.requestLocationUpdates(
-                            LocationManager.GPS_PROVIDER,
-                            MIN_TIME_UPDATES,
-                            MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
-
-                    if (locationManager != null) {
-                        location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-                        if (location != null) {
-                            latitude = location.getLatitude();
-                            longitude = location.getLongitude();
-                        }
-                    }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return location;
+    public GPS(Context c){
+        context = c;
     }
 
-    public void GPSstop(){
-        if(locationManager!=null){
-            locationManager.removeUpdates(GPS.this);
-        }
-    }
 
-    public double getLatitude(){
-        if(location !=null){
-            latitude = location.getLatitude();
-        }
-        return latitude;
-    }
-
-     public double getLongitude(){
-         if(location !=null){
-             longitude = location.getLongitude();
-         }
-         return longitude;
-     }
-
-     public boolean cgetLocation(){
-         return this.getLocation;
-     }
-
-     public void settingAlert(){
-         AlertDialog.Builder alertDialog  = new AlertDialog.Builder(context);
-         alertDialog.setTitle("GPS settings");
-         alertDialog.setMessage("GPS not enabled");
-         alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener(){
-             @Override
-             public void onClick(DialogInterface dialog, int which) {
-                 Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                 context.startActivity(intent);
-             }
-         });
-         alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-             @Override
-             public void onClick(DialogInterface dialog, int which) {
-                 dialog.cancel();
-             }
-         });
-         alertDialog.show();
-     }
 }
--- location_app_grp1/app/src/main/java/grp1/grp1/MapsActivity.java
+++ location_app_grp1/app/src/main/java/grp1/grp1/MapsActivity.java
@@ -2,7 +2,11 @@ package grp1.grp1;
 
 import android.Manifest;
 import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.graphics.drawable.RotateDrawable;
 import android.location.Address;
@@ -17,6 +21,7 @@ import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -35,7 +40,10 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.BufferedReader;
@@ -57,7 +65,7 @@ import com.google.maps.android.heatmaps.Gradient;
 import com.google.maps.android.heatmaps.HeatmapTileProvider;
 import com.google.maps.android.heatmaps.WeightedLatLng;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback{
 
     private GoogleMap mMap;
     private LocationManager locationManager;
@@ -67,13 +75,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private HeatmapTileProvider hmProvider;
     private TileOverlay mOverlay;
 
-    Button currentl, heatmapb1;
+    Button currentl, heatmapb1, crimeb;
+    Spinner spinnerm, spinnery;
     GPS gps;
     TextView laview, lnview;
-    JSONArray jsonArray;
-    ArrayList<LatLng> slocations = new ArrayList();
-    ArrayList<LatLng> mlocations = new ArrayList();
-    ArrayList<LatLng> hlocations = new ArrayList();
+    JSONArray jsonArray, jsonArray2;
+    //ArrayList<LatLng> slocations = new ArrayList();   //array for fixed (non dynamic) heatmap, small price
+   // ArrayList<LatLng> mlocations = new ArrayList();   //array for fixed (non dynamic) heatmap, medium price
+   // ArrayList<LatLng> hlocations = new ArrayList();   //array for fixed (non dynamic) heatmap high price
+    ArrayList<LatLng> clocations = new ArrayList(); //store crime location
+    ArrayList<CData> cdata = new ArrayList<>();     //store crime data
+    ArrayList<CData> cdata2 = new ArrayList<>();    //store crime data
+    ArrayList<Marker> markerremove = new ArrayList<>();     //store markers to remove
+    ArrayList<WeightedLatLng> wlocations = new ArrayList();     //weighted locations for dynamic heatmap
+    private HeatmapTileProvider mProvider;
+
+    double defaultlat = 50.82253000000001, defaultlong = -0.13716299999998682;  //default location if not sending latlong to app
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,72 +105,66 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         heatmapb1 = (Button) findViewById(R.id.hmb);
         laview = (TextView) findViewById(R.id.latview);
         lnview = (TextView) findViewById(R.id.lngview);
+        spinnerm = (Spinner) findViewById(R.id.cmonth);
+        spinnery = (Spinner) findViewById(R.id.cyear);
+        crimeb = (Button) findViewById(R.id.cdatab);
 
 
+        //get current location
+        ActivityCompat.requestPermissions(MapsActivity.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 111);
         currentl.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //calling GPS class method
                 gps = new GPS(MapsActivity.this);
                 double latitude, longitude;
-                if(gps.cgetLocation()){
-//                    latitude = gps.getLatitude();
-//                    longitude = gps.getLongitude();
-                    latitude = 50.8388481140;
-                    longitude = -0.1175390035;
-                    //laview.setText(""+latitude+"");
-                    lnview.setText(""+longitude+"");
+                Location location = gps.getLocation();
+                if(location!=null){
+                    //latitude = 50.82253000000001;
+                   // longitude = -0.13716299999998682;
+                    latitude = location.getLatitude();
+                    longitude = location.getLongitude();
+                    laview.setText(""+latitude+"");
+                    lnview.setText("" + longitude + "");
+                    defaultlat = latitude;
+                    defaultlong = longitude;
+                    //displaying current location
                     mMap.clear();
                     LatLng latlng = new LatLng(latitude, longitude);
-                    mMap.addMarker(new MarkerOptions().position(latlng).title("Current Position"));
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 10));
-
-                    //pushing to database
-                    String latstr= Double.toString(latitude);
-                    String langstr= Double.toString(longitude);
-                    String didstr = Integer.toString(1);
-                    Response.Listener<String> responseListener = new Response.Listener<String>() {
-                        @Override
-                        public void onResponse(String response) {
-                            try{
-                                JSONObject jsonObject = new JSONObject(response);
-                                boolean success = jsonObject.getBoolean("success");
-
-                                if(success){
-                                    Toast.makeText(getApplicationContext(), "Success", Toast.LENGTH_SHORT).show();
-                                }
-                            }catch (JSONException e){
-                                e.printStackTrace();
-                            }
-                        }
-                    };
-                    Dbreq dbreq = new Dbreq(latstr,langstr, didstr, responseListener);
-                    RequestQueue queue = Volley.newRequestQueue(MapsActivity.this);
-                    queue.add(dbreq);
-                }else{
-                    gps.settingAlert();
+                    mMap.addMarker(new MarkerOptions().position(latlng).title("Current Position").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latlng, 12));
                 }
             }
         });
 
-
+        //heatmap button: get json from edited link and process data through GetDataTask() method
         heatmapb1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                double latitude1, longitude1;
-                latitude1 = 50.8388481140;
-                longitude1 = -0.1175390035;
-                String link = "http://ec2-35-176-136-57.eu-west-2.compute.amazonaws.com:/price-data/get\\?lat="+latitude1+"&long="+longitude1+"&distance=1";
+                String link = "http://ec2-35-176-136-57.eu-west-2.compute.amazonaws.com:/price-data/get\\?lat="+defaultlat+"&long="+defaultlong+"&distance=2";
                 new GetDataTask().execute(link);
             };
         });
+
+        //crime button: get json from edited link(applying selected date) and process data through GetCrimeData() method
+        crimeb.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String month, year;
+                month = String.valueOf(spinnerm.getSelectedItem());
+                year = String.valueOf(spinnery.getSelectedItem());
+                String link = "https://data.police.uk/api/crimes-street/all-crime?lat="+defaultlat+"&lng="+defaultlong+"&date="+year+"-"+month;
+                new GetCrimeData().execute(link);
+            }
+        });
     }
 
-
+    //processing house price data and storing on wlocations array
     private void addLocation() {
-        slocations.clear();
-        mlocations.clear();
-        hlocations.clear();
-        System.out.println(jsonArray.length());
+        //slocations.clear();
+        //mlocations.clear();
+        //hlocations.clear();
+        wlocations.clear();
         if(jsonArray.length()!=0){
             for (int i = 0; i < 3000; i++) {
                 try {
@@ -161,14 +172,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     int price = jsonObject.getInt("price");
                     double lat = jsonObject.getDouble("lat");
                     double lng = jsonObject.getDouble("lng");
-                    System.out.println(jsonObject.toString());
-                    if(price<=100000){
+                    wlocations.add(new WeightedLatLng(new LatLng(lat, lng), Math.log(price)));
+                    /*if(price<=100000){
                         slocations.add(new LatLng(lat,lng));
                     }else if((price>100000)&&(price<=200000)){
                         mlocations.add(new LatLng(lat,lng));
                     }else if(price>200000){
                         hlocations.add(new LatLng(lat,lng));
-                    }
+                    }*/
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
@@ -176,14 +187,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    //processing crime data and storing on cdata array
+    private void addCrime(){
+        cdata2= cdata;
+        cdata.clear();
+        int jsonArray2length;
+        //remove if statement for full crime data, keep else statement
+        if(jsonArray2.length()>500){
+            jsonArray2length=500;
+        }else{
+            jsonArray2length=jsonArray2.length();
+        }
+        if(jsonArray2.length()!=0){
+            for(int i=0; i<jsonArray2length; i++){//jsonArray2.length()
+                try{
+                    JSONObject jsonObject = jsonArray2.getJSONObject(i);
+                    String catogery = jsonObject.getString("category");
+                    JSONObject jsonObject1 = jsonObject.getJSONObject("location");
+                    double lat = jsonObject1.getDouble("latitude");
+                    double lng = jsonObject1.getDouble("longitude");
+                    //System.out.println(lat);
+                    clocations.add(new LatLng(lat,lng));
+                    cdata.add(new CData((new LatLng(lat,lng)), catogery));
+                }catch (JSONException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 
+    //adding crime marker on map after removing previous markers
+    public void addCrimeMarker(){
+        if(cdata2.size()!=0){
+            for(int i=0; i<markerremove.size();i++){
+                Marker marker2 = markerremove.get(i);
+                marker2.remove();
+            }
+        }
+        LatLng curLocation = new LatLng(defaultlat,defaultlong);
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curLocation,14));
+        markerremove.clear();
+        for(int i=0; i<cdata.size(); i++) {
+            CData cdatalist = cdata.get(i);
+            //mMap.addMarker(new MarkerOptions().position(cdatalist.clatlng).title(cdatalist.cat));
+            Marker marker = mMap.addMarker(new MarkerOptions().position(cdatalist.clatlng).title(cdatalist.cat).alpha(0.5f).icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_action_name)));
+            markerremove.add(marker);
+        }
+    }
 
+    //adding weighted heatmap
     private void addHeatMap(){
-        int[] colors = {Color.rgb(102,225,0), Color.rgb(102,225,0)}; //green
+        int[] colors = {Color.rgb(0,25,240), Color.rgb(0,255,0), Color.rgb(240,255,0), Color.rgb(255,0,0)};
+        float[] startPoints = {
+                0.2f, 0.4f, 0.6f, 1f
+        };
+        Gradient gradient = new Gradient(colors, startPoints);
+        LatLng curLocation = new LatLng(defaultlat,defaultlong);
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(curLocation));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(curLocation,13));
+        mProvider = new HeatmapTileProvider.Builder().weightedData(wlocations).gradient(gradient).radius(27).opacity(0.9).build();
+        mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(mProvider));
+
+        //below code for non dynamic and fixed heatmap
+        /*int[] colors = {Color.rgb(102,225,0), Color.rgb(102,225,0)}; //green
         int[] colors2 = {Color.rgb(225,225,0), Color.rgb(225,225,0)}; //yellow
         int[] colors3 = {Color.rgb(225,0,0), Color.rgb(225,0,0)}; //red
         float[] startPoints = {
-            0.2f, 1f
+                0.2f, 1f
         };
         Gradient gradient1 = new Gradient(colors, startPoints);
         Gradient gradient2 = new Gradient(colors2, startPoints);
@@ -199,20 +270,74 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         if(!hlocations.isEmpty()) {
             hmProvider = new HeatmapTileProvider.Builder().data(hlocations).gradient(gradient3).build();
             mOverlay = mMap.addTileOverlay(new TileOverlayOptions().tileProvider(hmProvider));
-        }
+        }*/
     }
 
+
+    //on start
     @Override
     public void onMapReady(GoogleMap googleMap) {
         //method displays University of Sussex on the map on app startup
         mMap = googleMap;
         LatLng uos = new LatLng(50.867090, -0.087914);
-        mMap.addMarker(new MarkerOptions().position(uos).title("University of Sussex"));
+        mMap.addMarker(new MarkerOptions().position(uos).title("University of Sussex").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(uos));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(uos,12));
     }
 
-     class GetDataTask extends AsyncTask<String, Void, String> {
+    //posting data and reading input from server for house data
+    class GetDataTask extends AsyncTask<String, Void, String> {
+        ProgressDialog progressDialog;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MapsActivity.this);
+            progressDialog.setMessage("Loading data");
+            progressDialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            StringBuilder result = new StringBuilder();
+            //connect to server
+            try{
+                URL url = new URL(params[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection)url.openConnection();
+                urlConnection.setReadTimeout(10000); //ms
+                urlConnection.setConnectTimeout(10000); //ms
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setRequestProperty("Content-Type", "application/json");
+                urlConnection.connect();
+
+                //server response
+                InputStream inputStream = urlConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+                String line;
+                line=bufferedReader.readLine();
+                jsonArray = new JSONArray(line);
+                addLocation();
+            }catch (IOException e){
+                return "Network error!";
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return "";
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            addHeatMap();
+            if (progressDialog!=null){
+                progressDialog.dismiss();
+            }
+        }
+    }
+
+
+    //posting data and reading input from server for crime data
+    class GetCrimeData extends AsyncTask<String, Void, String> {
         ProgressDialog progressDialog;
 
         @Override
@@ -242,31 +367,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
                 String line;
                 line=bufferedReader.readLine();
-               // while((line=bufferedReader.readLine())!=null){
-                    //result.append(line).append("\n");
-                jsonArray = new JSONArray(line);
-                    System.out.println("-------------------------");
-                    System.out.println(jsonArray.length());
-                addLocation();
+                jsonArray2 = new JSONArray(line);
+                System.out.println("-------------------------");
+                addCrime();
             }catch (IOException e){
                 return "Network error!";
             } catch (JSONException e) {
                 e.printStackTrace();
             }
-            //test
-            /*try {
-                JSONObject jsonObject = jsonArray.getJSONObject(1);
+
+            try {
+                JSONObject jsonObject = jsonArray2.getJSONObject(1);
                 System.out.println(jsonObject.toString());
             } catch (JSONException e) {
                 e.printStackTrace();
-            }*/
+            }
             return "";
         }
 
         @Override
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
-            addHeatMap();
+            addCrimeMarker();
+            //addHeatMap();
             if (progressDialog!=null){
                 progressDialog.dismiss();
             }
